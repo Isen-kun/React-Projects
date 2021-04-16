54ee590babe359f537cbb99896dd29dbe5961c0f
Isen-kun <60184753+Isen-kun@users.noreply.github.com>
2021-04-16 11:49:41 +0530

    completed context app

--- context-app/src/components/Navbar.js
+++ context-app/src/components/Navbar.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 class Navbar extends Component {
   render() {
     return (
-      <nav>
-        <h1>Context App</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
+      <AuthContext.Consumer>
+        {(authcontext) => (
+          <ThemeContext.Consumer>
+            {(themecontext) => {
+              const { isAuthenticated, toggleAuth } = authcontext;
+              const { isLightTheme, light, dark } = themecontext;
+              const theme = isLightTheme ? light : dark;
+              return (
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                  <h1>Context App</h1>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? "Logged In" : "Logged Out"}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
